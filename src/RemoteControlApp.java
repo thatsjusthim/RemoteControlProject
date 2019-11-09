ff3004dba66e85309d3a8105333b3e14ba6aa989
Brandon Frazier <brandonfrazier@Brucebs-MacBook-Pro.local>
2019-11-08 17:24:08 -0700

    added the list of buttons without RC styling

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -2,7 +2,38 @@
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		System.out.print("To begin, please turn on the remote by pressing the ON button.");
+		
+		String button = scanner.next();
+		
+		switch (button) {
+			case "0":
+			case "1":
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	// TODO: Add pressButton() method
 	// TODO: Add turnOn() method
