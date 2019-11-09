e4b92c71e747cd0abefaf2f0c5a837a6c30b26b1
Brandon Frazier <brandonfrazier@Brucebs-MacBook-Pro.local>
2019-11-09 10:54:13 -0700

    added more buttons and transferred ON/OFF buttons from reference package

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,10 +1,13 @@
 
-public class RemoteControlApp {
-
-	public static void main(String[] args) {
+public class RemoteControlApp; {
+	
+	import java.util.Scanner; 
+	
+	public static @ main(String[] args) {
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
 		System.out.print("To begin, please turn on the remote by pressing the ON button.");
-		
+		System.out.print();
+		Scanner scanner;
 		String button = scanner.next();
 		
 		switch (button) {
@@ -18,23 +21,26 @@ public class RemoteControlApp {
 			case "7":
 			case "8":
 			case "9":
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+				
+				
+		 scanner.close(); }
+		  
 	}
-	// TODO: Add pressButton() method
-	// TODO: Add turnOn() method
-}
+			private static void turnOn() {
+		    System.out.println("TURNING ON...");
+
+		  }
+
+			private static void turnOff() {
+		    System.out.println("TURNING OFF...");
+		  }
+
+			public static void pressButton() {
+		    System.out.println("BOOP!");
+		  }
+
+			public static void error() {
+		    System.out.println("Command not recognized.");
+		  }
+		}
+
